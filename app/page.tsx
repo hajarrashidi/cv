700099b35ebb030c09e95160706311d28c2bbbf3
Hajar <hajarrashidi@gmail.com>
2024-08-26 16:42:54 +0200

    design fixes

--- app/page.tsx
+++ app/page.tsx
@@ -84,28 +84,28 @@ export default async function Page() {
     <main className="m-0 p-0 bg-gray-50 min-h-screen">
       {/* Container to center-align content */}
       <div className="max-w-3xl mx-auto">
-        <h1 className="text-4xl font-bold p-6 text-center">{cv.fullName}</h1>
-        <h1 className="text-md pb-2 text-center text-gray-600">Experience</h1>
+        <h1 className="text-4xl font-bold pt-8 pb-4 text-center pl-6">{cv.fullName}</h1>
+        <h1 className="text-md pb-2 text-center text-gray-600 pl-7">Experience</h1>
 
         <div className="relative">
-          <div className="border-l-2 rounded border-gray-200 absolute left-8 top-0 h-full"></div>
+          <div className="border-l-2 rounded border-gray-200 absolute left-7 top-0 h-full"></div>
           {cv.workExperience.map((experience, index) => (
-            <div key={index} className="relative pb-2 pl-16 pr-8">
+            <div key={index} className="relative pb-2 pl-14 pr-4">
               <div className="bg-white rounded-lg border border-gray-200 p-3.5">
-                <h2 className="text-sm md:text-md font-semibold flex items-center">
-                  <span className="text-sm md:text-md">{experience.position}</span>
+                <h2 className="text-sm font-semibold flex items-center pb-1">
+                  <span>{experience.position}</span>
                   <span className="mx-2 border-l-2 h-4 rounded border-grey-100"></span>
-                  <span className="text-sm md:text-md">{experience.company}</span>
+                  <span>{experience.company}</span>
                 </h2>
 
-                <p className="text-xs md:text-sm text-gray-700">
+                <p className="text-xs text-gray-700">
                   {experience.startDate} - {experience.endDate}
                 </p>
                 <p className="text-xs text-gray-500">{experience.location}</p>
 
                 {experience.assignments && experience.assignments.length > 0 && (
                   <div className="mt-3">
-                    <h3 className="text-xs md:text-sm font-medium pb-1">Assignments</h3>
+                    <h3 className="text-xs font-medium pb-1">Assignments</h3>
                     {experience.assignments.map((assignment, idx) => (
                       <div key={idx} className="pt-1 pb-1">
                         <p className="text-xs text-gray-700">{assignment.title}</p>
@@ -114,7 +114,7 @@ export default async function Page() {
                   </div>
                 )}
               </div>
-              <div className="absolute left-4 top-1/2 w-8 h-8 bg-gray-200 rounded-full transform -translate-y-1/2">
+              <div className="absolute left-3 top-1/2 w-8 h-8 bg-gray-200 rounded-full transform -translate-y-1/2">
                 {experience.logotype ? (
                   <img
                     src={experience.logotype}
@@ -127,19 +127,19 @@ export default async function Page() {
           ))}
         </div>
 
-        <h1 className="text-md p-3 text-center text-gray-600">Education</h1>
+        <h1 className="text-md p-3 text-center text-gray-600 pl-7">Education</h1>
 
         <div className="relative">
-          <div className="border-l-2 border-gray-200 absolute left-8 top-0 h-full"></div>
+          <div className="border-l-2 rounded border-gray-200 absolute left-7 top-0 h-full"></div>
           {cv.education.map((edu, index) => (
-            <div key={index} className="relative pb-2 pl-16 pr-8">
+            <div key={index} className="relative pb-2 pl-14 pr-4">
               <div className="bg-white rounded-lg border border-gray-200 p-3.5">
-                <h2 className="font-semibold flex items-center">
-                  <span className="text-xs ">{edu.degree}</span>
+                <h2 className="text-sm font-semibold flex items-center pb-1">
+                  <span>{edu.degree}</span>
                   <span className="mx-2 border-l-2 h-4 rounded border-grey-100"></span>
-                  <span className="text-xs">{edu.institution}</span>
+                  <span>{edu.institution}</span>
                 </h2>
-                <p className="text-xs md:text-sm text-gray-700">
+                <p className="text-xs text-gray-700">
                   {edu.startDate} - {edu.endDate}
                 </p>
                 <p className="text-xs text-gray-500">{edu.location}</p>
